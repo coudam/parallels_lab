ad8046e689d10043a785507d7460ba6a5c180cb2
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:00:26 +0300

    Scripted auto-commit on change (2020-12-25 20:00:26) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
+++ lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
@@ -15,6 +15,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
