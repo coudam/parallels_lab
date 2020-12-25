78e73668a45fea42deceb7a4b7d96bedbe0ba10e
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:02:36 +0300

    Scripted auto-commit on change (2020-12-25 20:02:36) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
+++ lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
@@ -27,6 +27,6 @@ public class AirportsStatsRun {
         job.setJobName("Stat");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportTableMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportTableMapper.class);
-        FileOutputFormat.setOutputPath(jod, );
+        FileOutputFormat.setOutputPath(job, new Path());
     }
 }
