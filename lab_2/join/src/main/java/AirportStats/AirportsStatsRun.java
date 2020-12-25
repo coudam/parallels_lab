6607cf8236cc7e12e08812a64c9c5215c79db5ed
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:00:00 +0300

    Scripted auto-commit on change (2020-12-25 20:00:00) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
+++ lab_2/join/src/main/java/AirportStats/AirportsStatsRun.java
@@ -24,6 +24,6 @@ public class AirportsStatsRun {
         Job job = Job.getInstance(conf, "join");
         job.setJarByClass(AirportsStatsRun.class);
         job.setJobName("Stat");
-        MultipleInputs.addInputPath(job, new Path(args[0]), );
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, );
     }
 }
