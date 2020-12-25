54e9097d5fe59a0cf12ea8535e05c03e7cb0a179
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:21:23 +0300

    Scripted auto-commit on change (2020-12-25 20:21:23) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/TableReducer.java
+++ lab_2/join/src/main/java/AirportStats/TableReducer.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 public class TableReducer extends Reducer<AirportKey, Text, Text, Text> {
     public  void reduce(AirportKey key, Iterable<Text> valuse, Context context) throws IOException {
-        
+        int count = 0;
+        double min, max;
     }
 }
