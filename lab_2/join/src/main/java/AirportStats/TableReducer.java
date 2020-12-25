8e7cf1b4dea29b4e09b9da859628fe69db11f5c3
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:17:41 +0300

    Scripted auto-commit on change (2020-12-25 20:17:41) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/TableReducer.java
+++ lab_2/join/src/main/java/AirportStats/TableReducer.java
@@ -1,4 +1,8 @@
 package AirportStats;
 
-public class TableReducer {
+import org.apache.hadoop.mapreduce.Reducer;
+
+import javax.xml.soap.Text;
+
+public class TableReducer extends Reducer<AirportKey, Text, Text, Text> {
 }
