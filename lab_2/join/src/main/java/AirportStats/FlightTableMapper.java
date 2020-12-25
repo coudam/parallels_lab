b072cd5445a7164967c2184fe2cb1ca0aaf9643e
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:09:26 +0300

    Scripted auto-commit on change (2020-12-25 20:09:26) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/FlightTableMapper.java
+++ lab_2/join/src/main/java/AirportStats/FlightTableMapper.java
@@ -1,4 +1,15 @@
 package AirportStats;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import java.io.IOException;
 
-public class FlightTableMapper {
+public class FlightTableMapper extends Mapper<LongWritable, Text, AirportKey, Text> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String id_airpirt[] = CSVReader.readAirports(value);
+        AirportKey akey = new AirportKey(id_airpirt[0].replace("/", ""), 0);
+        context.write(akey, new Text(id_airpirt[1]));
+    }
 }
