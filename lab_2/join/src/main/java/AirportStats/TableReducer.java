0e2ca95ea5fbc22c6938bbbb29e755c27c7e3dd2
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 20:19:49 +0300

    Scripted auto-commit on change (2020-12-25 20:19:48) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/TableReducer.java
+++ lab_2/join/src/main/java/AirportStats/TableReducer.java
@@ -3,6 +3,10 @@ package AirportStats;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
 
+import java.io.IOException;
+
 public class TableReducer extends Reducer<AirportKey, Text, Text, Text> {
-    public  void reduce() 
+    public  void reduce(AirportKey key, Iterable<Text> valuse, Context context) throws IOException {
+        
+    }
 }
