667363ec7e50951d9dfed5db9d1ab15298b60ec5
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 17:49:46 +0300

    Scripted auto-commit on change (2020-12-25 17:49:46) by gitwatch.sh

--- lab_2/WordCount/src/main/java/counter/AirportKey.java
+++ lab_2/WordCount/src/main/java/counter/AirportKey.java
@@ -1,4 +1,11 @@
 package counter;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.Text;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportKey {
 }
