c4469797e5b26c79360f991f93189601704474a8
Alex Konyukhov <brfh-1999@bk.ru>
2020-12-25 19:33:43 +0300

    Scripted auto-commit on change (2020-12-25 19:33:43) by gitwatch.sh

--- lab_2/join/src/main/java/AirportStats/TablePartitioner.java
+++ lab_2/join/src/main/java/AirportStats/TablePartitioner.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class TablePartitioner extends Partitioner<AirportKey, Text>  {
-
     @Override
     public int getPartition(AirportKey airportKey, Text text, int i) {
         return 0;
